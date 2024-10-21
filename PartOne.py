9f5c98aced43b461fbc06c1421e0b203e20af332
jlb84 <jlb84@kent.ac.uk>
2024-10-21 10:00:02 +0100

    Update PartOne.py

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,12 @@
+camel_case_input = input("Enter a variable name in camel case: ")
+
+snake_case = ""
+
+for c in camel_case_input:
+    if c.isupper():
+        snake_case += "_" + c.lower()
+    else:
+        snake_case += c
+
+print("The variable name in snake case is:", snake_case)
 
